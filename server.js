9330b79ad438a0b9689168c2832c7fdff0500189
ParretLabs <parretlabs@gmail.com>
2022-01-29 15:48:04 -0500

    updated redirect url

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
 // Using CORS to allow the API to access the callback endpoint.
@@ -29,46 +29,46 @@ app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+	response.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/verified", (request, response) => {
-  response.sendFile(__dirname + "/views/verified.html");
+	response.sendFile(__dirname + "/views/verified.html");
 });
 
 // This endpoint has been set as the callback URL in the Settings Panel.
 app.post("/callback", (request, response) => {
-  console.log(request.body);
-  if(request.body.verified) {
-    // Send back the users profileID and their favorite animal.
-    response.json({
-      redirectURL: "https://ctfauth.glitch.com/verified#" + Buffer.from(request.body.profileID + "|" + verificationMap[request.body.verificationToken]).toString('base64')
-    });
-  }
+	console.log(request.body);
+	if(request.body.verified && request.body.apiKey === process.env.API_KEY) {
+		// Send back the users profileID and their favorite animal.
+		response.json({
+			redirectURL: "http://parretlabs.xyz:8015/#" + Buffer.from(request.body.profileID + "|" + verificationMap[request.body.verificationToken]).toString('base64')
+		});
+	}
 });
 
 // This endpoint stores the animal in the verification map for later use then gives the user the verification URL.
 app.post("/verify", (request, response) => {
-  if(request.body.animal) {
-    request.body.animal = String(request.body.animal).slice(0, 50);
-    fetch("https://ctfauth.herokuapp.com/api/v1/generate_verification_link", {
-      method: "POST",
-      headers: {
-        "Authorization": "Basic " + process.env.API_KEY
-      }
-    }).then(a => a.json()).then(json => {
-      verificationMap[json.verificationToken] = request.body.animal;
-      
-      console.log(json);
-      
-      response.json({
-        url: json.url
-      });
-    });
-  }
+	if(request.body.animal) {
+		request.body.animal = String(request.body.animal).slice(0, 50);
+		fetch("https://ctfauth.herokuapp.com/api/v1/generate_verification_link", {
+			method: "POST",
+			headers: {
+				"Authorization": "Basic " + process.env.API_KEY
+			}
+		}).then(a => a.json()).then(json => {
+			verificationMap[json.verificationToken] = request.body.animal;
+			
+			console.log(json);
+			
+			response.json({
+				url: json.url
+			});
+		});
+	}
 });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+	console.log("Your app is listening on port " + listener.address().port);
 });
