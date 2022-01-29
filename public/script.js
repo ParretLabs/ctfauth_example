115bce43d9b344323cb26a4632597e3fb4bb2cf4
Korbin.Deary45@gmail.com <korbin.deary45@gmail.com>
2022-01-29 15:46:48 -0500

    cleaned scripts and styles

--- public/script.js
+++ public/script.js
@@ -1,26 +1,20 @@
-// client-side js, loaded by index.html
-// run by the browser each time the page is loaded
-
-console.log("hello world :o");
-
-// define variables that reference elements on our page
 const verifyButton = document.getElementById("verifyBtn");
 const animalBox = document.getElementById("animalText");
 
 verifyButton.onclick = () => {
-  fetch("/verify", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      animal: animalBox.value.slice(0, 50)
-    })
-  }).then(a => a.json()).then(json => {
-    if(json.err) {
-      return alert("Error: " + json.err);
-    }
+	fetch("/verify", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			animal: animalBox.value.slice(0, 50)
+		})
+	}).then(a => a.json()).then(json => {
+		if(json.err) {
+			return alert("Error: " + json.err);
+		}
 
-    location.href = json.url;
-  });
+		location.href = json.url;
+	});
 }
